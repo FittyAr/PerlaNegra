fc11bc3aff9fee31703d1620e388c4d7d5575b41
Iván <10284757+FittyAr@users.noreply.github.com>
2024-12-21 18:26:49 -0300

    -
    -

--- home/views.py
+++
@@ -1,9 +0,0 @@
-"""Vistas para la aplicación home."""
-
-from django.shortcuts import render
-#from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    """Renderiza la página principal del sitio."""
-    return render(request, 'home/index.html')
